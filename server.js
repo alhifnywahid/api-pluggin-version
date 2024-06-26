926dee1186b60d54e8e78b0e91dc05ab00b30389
alhifnywahid <hifnycarlytosss@gmail.com>
2024-06-27 06:50:15 +0700

    testing ejs again

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
-require("dotenv").config();
-require("./lib/system/config");
+require("./lib/system/config"), require("dotenv").config(), require("./config/db")();
 const express = require("express");
 const cors = require("cors");
 const loadRouters = require("./routers");
-const connectDB = require("./config/db");
-
+const path = require("path");
+const port = process.env.PORT || 3000;
 const app = express();
-app.set("view engine", "ejs");
-
-app.get("/ejs", (req, res) => {
-	res.render("index", { title: "Home Page", message: "Welcome to Express with EJS!" });
-});
+loadRouters(app);
 
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-connectDB();
-
-loadRouters(app);
+app.get("/ejs", (req, res) => {
+	res.render("index", { title: "Home Page", message: "Welkombek again!" });
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
 
 app.use((err, req, res, next) => {
-	console.log(req)
 	console.error(err.stack);
-	res.status(500).send('Something broke!');
+	res.status(500).send("Something broke!");
 });
