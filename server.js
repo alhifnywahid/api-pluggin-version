94318b7d4a8c026ec6c12a68e48ececef62353cf
alhifnywahid <hifnycarlytosss@gmail.com>
2024-06-26 12:14:09 +0700

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors");
 const loadRouters = require('./routers');
 const connectDB = require('./config/db');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 
